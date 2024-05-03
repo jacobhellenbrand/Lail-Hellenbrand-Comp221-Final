dac0f50e12dd33979def21193554b6add153082d
jacobhellenbrand <112660239+jacobhellenbrand@users.noreply.github.com>
2024-05-03 18:11:49 -0500

    creating fft code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Lail-Hellenbrand-Comp221-Final
 Implementing the FFT 
+
+# This is our code for implenting the FFT!
+The implementation itself is meant for a very simple usage of the FFT. Anticipated inputs are just arrays of points that should be length that is equal to a power of 2. 
+ Running this code is super simple
+  * Open the repository and hit run
+  * In the console you should see a series of test cases running 
+    * These return true or false depending on if our code gives the anticipated output. 
+    * Look closely at outputs our code has some issues evaluating floating point numbers. As a result some rounding issues may result that could cause a test to return "False" even though a correct output is returned. 
+* If you would like to create your own test cases we used this website to determine what outputs should be for a series of points. 
+    * [FFT Calculator!](https://tonysader.github.io/FFT_Calculator/)
---
+++ main.py
@@ -0,0 +1,57 @@
+from math import sin,cos,pi
+
+# This is a FFT function that computes DFT of a sequence of numbers
+# Input is a list of numbers and using FFT, will return their DFT outputs
+def fft(a):
+	n = len(a)
+    #check base case
+	if n == 1:
+		return [a[0]]
+	# Make our complex unity numbers
+	theta = -2*pi/n
+	w = list( complex(cos(theta*i), sin(theta*i)) for i in range(n) ) 
+	# make even and odd parts 
+	a_Even = a[0::2]
+	a_Odd = a[1::2]
+	# Recursive calls on n/2 size
+	f_even = fft(a_Even) 
+	f_Odd = fft(a_Odd)
+	# Initialize output array
+	F = [0]*n
+	middle = n//2
+	for k in range(n//2):
+        #store even and odd parts 
+		w_yodd_k = w[k] * f_Odd[k]
+		yeven_k = f_even[k]
+		#compute values for out put array
+		F[k]		 = yeven_k + w_yodd_k
+		F[k + middle] = yeven_k - w_yodd_k
+        
+	F = [val.real if val.imag == 0 else val for val in F]
+	return F
+
+
+# Test cases
+def run_test(input_data, expected_output):
+    output = fft(input_data)
+    print("Input:", input_data)
+    print("Expected Output:", expected_output)
+    print("Actual Output:", output)
+    print("Test Passed:", output == expected_output)
+    print()
+
+
+if __name__ == '__main__':
+    # Test cases
+    #10, 0+4J, 0+2J, 0+4J, -10, 0+-4J, 0+-2J, 0+-4J
+    test_cases = [
+        ([1, 2, 3, 4], [10.0, (-2.0+2.0j), -2.0, (-2.0-2.0j)]),
+        ([1, 0, 0, 0, 2, 0, 0, 0], [3.0, (-1), (3), (-1), 3.0, (-1), (3), (-1)]),
+        ([0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0]),
+        ([1, 1, 1, 1], [4.0] + [0.0j] * 3)
+    ]
+
+    # Run test cases
+    for idx, (input_data, expected_output) in enumerate(test_cases, start=1):
+        print("Test Case", idx)
+        run_test(input_data, expected_output)
